7e87a0374beda12928545d7f33a21326e4b1c8e3
Yiheng Du <15700066418@163.com>
2020-05-30 16:54:06 +0800

    fix harvester bug

--- role.harvester.js
+++ role.harvester.js
@@ -18,6 +18,7 @@ var roleHarvester = {
 
         if(creep.state == 'arrive') {
             const path = creep.pos.findPathTo(target);
+            console.log(path);
             if(path.length > 0) {
                 creep.move(path[0].direction);
             }
