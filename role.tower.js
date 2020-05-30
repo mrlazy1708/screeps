8a2bd1b3a8acab3a0cf3e93c9a155f8145965489
Yiheng Du <15700066418@163.com>
2020-05-30 14:45:47 +0800

    debug

--- role.tower.js
+++ role.tower.js
@@ -1,9 +1,9 @@
 var roleTower = {
     run: function(tower) {
         if(tower.store[RESOURCE_ENERGY] >= 10) {
-            var target = tower.pos.findClosestByRange(FIND_MY_STRUCTURES, {
+            var target = tower.pos.findClosestByRange(FIND_STRUCTURES, {
                 filter: function(object) {
-                    return object.structureType == STRUCTURE_ROAD;
+                    return object.hitsMax - object.hits >= 10;
                 }
             });
             console.log(target);
