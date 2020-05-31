6accbde1f23f11618e37a59674274050f8111f8b
Yiheng Du <15700066418@163.com>
2020-05-31 14:51:20 +0800

    change flee range

--- task.defense.js
+++ task.defense.js
@@ -2,7 +2,7 @@ var taskDefense = {
     run: function(enemys) {
         for(var name in Game.creeps) {
             var creep = Game.creeps[name];
-            var path = PathFinder.search(creep.pos, enemys.map(c=>{ return{ pos: c.pos, range: 15 } }), { flee: true }).path;
+            var path = PathFinder.search(creep.pos, enemys.map(c=>{ return{ pos: c.pos, range: 10 } }), { flee: true }).path;
             if(path.length > 0) {
                 creep.moveByPath(path);
             }
