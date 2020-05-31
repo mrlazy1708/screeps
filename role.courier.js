97abf1e7b62165231c8741c86a14f00f4d3d8457
Yiheng Du <15700066418@163.com>
2020-05-31 12:33:25 +0800

    change courier value

--- role.courier.js
+++ role.courier.js
@@ -88,7 +88,7 @@ var roleCourier = {
                     if(target == null) {
                         target = creep.pos.findClosestByPath(FIND_MY_CREEPS, {
                             filter: function(object) {
-                                return object.memory.role == 'worker' && object.store[RESOURCE_ENERGY] + object.memory.reserved < ( Math.abs(object.pos.x - creep.pos.x) + Math.abs(object.pos.y - creep.pos.y) )*20;
+                                return object.memory.role == 'worker' && object.store[RESOURCE_ENERGY] + object.memory.reserved < ( Math.abs(object.pos.x - creep.pos.x) + Math.abs(object.pos.y - creep.pos.y) )*4;
                             }
                         });
                         if(target == null) {
