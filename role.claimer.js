42e89fab0f1db0974ab38a91bbc8b9f023400cd8
Yiheng Du <15700066418@163.com>
2020-06-01 21:59:20 +0800

    fix claimer

--- role.claimer.js
+++ role.claimer.js
@@ -2,7 +2,7 @@ var roleClaimer = {
     run: function(creep) {
         
         var target = Game.getObjectById('5bbcaf9b9099fc012e63ade8');
-        creep.moveTo(new RoomPosition(36,14,'E44S32'));
+        //creep.moveTo(new RoomPosition(36,14,'E44S32'));
         console.log(target);
         if(creep.attackController(target) == ERR_NOT_IN_RANGE) {
             creep.moveTo(target);
