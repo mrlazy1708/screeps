8613b145e585f42ae88d4a40b0275d59693b66b3
Yiheng Du <15700066418@163.com>
2020-06-01 22:55:47 +0800

    fdebug harvester

--- role.harvester.js
+++ role.harvester.js
@@ -2,31 +2,53 @@ var roleHarvester = {
     run: function(creep) {
         var target = Game.getObjectById(creep.memory.targetID);
         if(creep.memory.state == 'idle') {
-            var freeContainers = creep.room.find(FIND_STRUCTURES, {
-                filter: function(object){
-                    return object.structureType == STRUCTURE_CONTAINER && Game.getObjectById(object.memory.hostID) == null;
+            /*var room = Game.getObjectById(creep.memory.roomID);
+            if(creep.room == room) {*/
+                var freeContainers = creep.room.find(FIND_STRUCTURES, {
+                    filter: function(object){
+                        return object.structureType == STRUCTURE_CONTAINER && Game.getObjectById(object.memory.hostID) == null;
+                    }
+                });
+                if(freeContainers.length > 0) {
+                    target = freeContainers[0].pos.findClosestByRange(FIND_SOURCES);
+                    freeContainers[0].memory.hostID = creep.id;
+                    console.log(freeContainers[0].memory.hostID);
+                    creep.memory.containerID = freeContainers[0].id;
+                    creep.memory.targetID = target.id;
+                }/*
+                else {
+                    target = creep.room.findClosestByPath(FIND_SOURCES);
+                    creep.memory.targetID = target.id;
                 }
-            });
-            if(freeContainers.length > 0) {
-                target = freeContainers[0].pos.findClosestByRange(FIND_SOURCES);
-                freeContainers[0].memory.hostID = creep.id;
-                creep.memory.containerID = freeContainers[0].id;
-                creep.memory.targetID = target.id;
-                creep.memory.state = 'arrive';
             }
             else {
-                creep.say('💤');
-            }
+                const route = Game.map.findRoute(creep.room, room);
+                target = creep.pos.findClosestByPath(route[0].exit);
+            }*/
+            creep.memory.state = 'arrive';
         }
 
         if(creep.memory.state == 'arrive') {
-            const path = creep.pos.findPathTo(Game.getObjectById(creep.memory.containerID));
-            if(path.length > 0) {
-                creep.move(path[0].direction);
-                creep.say('🎯');
+            var container = Game.getObjectById(creep.memory.containerID);
+            if(container) {
+                const path = creep.pos.findPathTo(container);
+                if(path.length > 0) {
+                    creep.move(path[0].direction);
+                    creep.say('🎯');
+                }
+                else {
+                    creep.memory.state = 'work';
+                }
             }
             else {
-                creep.memory.state = 'work';
+                const path = creep.pos.findPathTo(target);
+                if(path.length > 1) {
+                    creep.move(path[0].direction);
+                    creep.say('🎯');                    
+                }
+                else {
+                    creep.memory.state = 'work';
+                }
             }
         }
         
