f4ce7acfa987de7555ae12e2ccd9df5cee94ff2a
Yiheng Du <15700066418@163.com>
2020-06-02 11:38:54 +0800

    restore stable version

--- task.upgrade.js
+++ task.upgrade.js
@@ -20,7 +20,7 @@ var taskUpgrade = {
                     }
                 }
                 delete Memory.creeps[name];
-                Game.spawns['Spawn1'].memory.death++;
+                Memory.nDeath++;
                 continue;
             }
 
