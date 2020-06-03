ecc34bd80fee046a19af86196395e0394e74f013
Yiheng Du <15700066418@163.com>
2020-06-03 20:23:09 +0800

    fix bug

---
+++ role.traveler.js
@@ -0,0 +1,23 @@
+const roleTraveler = {
+    run: function(creep) {
+
+        if(creep.memory.state == 'idle') {
+            const route = Game.map.findRoute(creep.room, creep.memory.roomName);
+            creep.memory.targetPos = creep.pos.findClosestByPath(route[0].exit);
+            creep.memory.state = 'travel';
+        }
+
+        if(creep.memory.state == 'travel') {
+            const path = creep.pos.findPathTo(creep.memory.targetPos);
+            creep.move(path[0].direction);
+            if(path.length == 1) {
+                creep.memory.state = 'idle';
+            }
+            else {
+                creep.say('🚌');
+            }
+        }
+    }
+};
+
+module.exports = roleTraveler;
