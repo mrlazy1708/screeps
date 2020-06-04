327087a625c23905a2ccaf71638e5f338d776b26
Yiheng Du <15700066418@163.com>
2020-06-04 21:24:29 +0800

    fix fatal bug

--- role.traveler.js
+++ role.traveler.js
@@ -1,39 +1,34 @@
 const roleTraveler = {
     run: function(creep) {
 
-        if(creep.memory.state == 'idle') {
-            const target = creep.pos.findClosestByPath(Game.map.findRoute(creep.room, creep.memory.roomName)[0].exit);
-            creep.memory.x = target.x;
-            creep.memory.y = target.y;
-            creep.memory.state = 'travel';
+        if(creep.memory.curRoom == creep.room.name) {
+            creep.moveTo(new RoomPosition(creep.memory.x, creep.memory.y, creep.room.name));
+            creep.say('🚌');
         }
-
-        if(creep.memory.state == 'travel') {
-            if(creep.memory.curRoom == creep.room.name) {
-                creep.moveTo(new RoomPosition(creep.memory.x, creep.memory.y, creep.room.name));
-                creep.say('🚌');
+        else {
+            creep.memory.curRoom = creep.room.name;
+            if(creep.memory.curRoom == creep.memory.roomName) {
+                delete creep.memory.curRoom;
+                if(creep.pos.x == 0) {
+                    creep.move(RIGHT);
+                }
+                if(creep.pos.x == 49) {
+                    creep.move(LEFT);
+                }
+                if(creep.pos.y == 0) {
+                    creep.move(TOP);
+                }
+                if(creep.pos.y == 49) {
+                    creep.move(BOTTOM);
+                }
+                creep.memory.state = 'idle';
+                creep.say('🗽');
             }
             else {
-                creep.memory.state = 'idle';
-                creep.memory.curRoom = creep.room.name;
-                if(creep.memory.curRoom == creep.memory.roomName) {
-                    if(creep.pos.x == 0) {
-                        creep.move(RIGHT);
-                    }
-                    if(creep.pos.x == 49) {
-                        creep.move(LEFT);
-                    }
-                    if(creep.pos.y == 0) {
-                        creep.move(TOP);
-                    }
-                    if(creep.pos.y == 49) {
-                        creep.move(BOTTOM);
-                    }
-                    creep.say('🗽');
-                }
-                else {
-                    creep.say('💤');
-                }
+                const target = creep.pos.findClosestByPath(Game.map.findRoute(creep.room, creep.memory.roomName)[0].exit);
+                creep.memory.x = target.x;
+                creep.memory.y = target.y;
+                creep.say('💤');
             }
         }
     }
--- task.creep.js
+++ task.creep.js
@@ -15,7 +15,7 @@ const taskCreep = {
 
             if(creep != null) {//ticks to live
                 if(creep.painc != Game.time){
-                    if(creep.memory.curRoom == creep.memory.roomName) {
+                    if(creep.memory.state != 'travel') {
                         if(creep.memory.role == 'courier') {
                             roleCourier.run(creep);
                             if(creep.memory.state != 'get' && creep.memory.state != 'give') {
@@ -62,16 +62,16 @@ const taskCreep = {
 const room = Game.spawns['Spawn1'].room;
         if(!Game.spawns['Spawn1'].spawning) {
             if(nJack == 0) {
-                Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Jack_'+Game.time, {memory: {role: 'jack', roomName: 'E44S32', state: 'idle'}});
+                Game.spawns['Spawn1'].spawnCreep([WORK, CARRY, MOVE], 'Jack_'+Game.time, {memory: {role: 'jack', roomName: 'E44S32', state: 'travel'}});
             }
             else if(nHarvester < 2 && nCourier >= 1) {
-                Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE], 'Harvester_'+Game.time, {memory: {role: 'harvester', roomName: room.name, state: 'idle', reserved: 0}});
+                Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, WORK, WORK, MOVE, MOVE, MOVE], 'Harvester_'+Game.time, {memory: {role: 'harvester', roomName: room.name, state: 'travel', reserved: 0}});
             }
             else if(nFreeCourier < 1) {
-                Game.spawns['Spawn1'].spawnCreep([CARRY, CARRY, MOVE, MOVE], 'Courier_'+Game.time, {memory: {role: 'courier', roomName: room.name, state: 'idle', reserved: 0}});
+                Game.spawns['Spawn1'].spawnCreep([CARRY, CARRY, MOVE, MOVE], 'Courier_'+Game.time, {memory: {role: 'courier', roomName: room.name, state: 'travel', reserved: 0}});
             }
             else if(nWorker < 4) {
-                Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], 'Worker_'+Game.time, {memory: {role: 'worker',roomName: room.name, state: 'idle', reserved: 0}});
+                Game.spawns['Spawn1'].spawnCreep([WORK, WORK, WORK, WORK, CARRY, CARRY, CARRY, CARRY, MOVE, MOVE, MOVE, MOVE], 'Worker_'+Game.time, {memory: {role: 'worker',roomName: room.name, state: 'travel', reserved: 0}});
             }
         }
 
