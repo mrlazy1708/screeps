c02274a4e1c3fa3701d2c1ab838625c6267286bd
Yiheng Du <15700066418@163.com>
2020-06-04 17:59:40 +0800

    organize code

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const taskCreep = require('task.creep');
 const taskExamine = require('task.examine');
 const taskInit = require('task.init');
 const taskNotify = require('task.notify');
+const taskRoom = require('task.room');
 
 profiler.enable();
 
@@ -11,37 +12,7 @@ module.exports.loop = function () {
 	// profiler.wrap(function() {
 		taskInit.run();
 
-		for(let name in Game.rooms) {
-			let room = Game.rooms[name];
-			const enemys = room.find(FIND_HOSTILE_CREEPS);
-			if(enemys.length) {
-				if(!room.memory.invaded) {
-					room.memory.invaded = true;
-					Memory.message += Game.time+': '+enemys.length+enemys[0].owner.username+'\'s creep'+(enemys.length==1?'':'s')+' in '+room.name+'\n';
-					console.log('Room '+room.name+' is under attack!');
-				}
-				enemys.forEach(function(object) {
-					let victims = object.pos.findInRange(FIND_MY_CREEPS, 5, {
-						filter: function(object) {
-							return object.memory.painc != Game.time;
-						}
-					});
-					victims.forEach(function(creep) {
-						const path = PathFinder.search(creep.pos, enemys.map(object=>{return{pos: object.pos, range: 5}}), {flee: true}).path;
-						creep.move(path[0].direction);
-						creep.memory.panic = Game.time;
-						creep.say('🐥');
-					});
-				});
-			}
-			else {
-				if(room.memory.invaded) {
-					room.memory.invaded = false;
-					taskExamine.run(room);
-					console.log('Enemys slayed in room '+room.name+' !');
-				}
-			}
-		}
+		taskRoom.run();
 
 		taskCreep.run();
 
