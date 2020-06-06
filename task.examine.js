20bf7515105d6b8650586ccad68bccc29cc1d2c8
Yiheng Du <15700066418@163.com>
2020-06-06 19:59:27 +0800

    add examine

--- task.examine.js
+++ task.examine.js
@@ -1,19 +1,21 @@
 const taskExamine = {
     run: function(room) {
-        const structures = room.find(FIND_STRUCTURES);
-        Memory.containers = [];
-        for(let index in structures) {
-            const structure = structures[index];
-            if(structure.structureType == STRUCTURE_EXTENSION) {
-                if(structure.memory.reserved = undefined) {
-                    structure.memory.reserved = 0;
+        for(let name in Game.rooms) {
+            let room = Game.rooms[name], structures = room.find(FIND_STRUCTURES);
+            Memory.containers = [];
+            for(let index in structures) {
+                const structure = structures[index];
+                if(structure.structureType == STRUCTURE_EXTENSION) {
+                    if(structure.memory.reserved = undefined) {
+                        structure.memory.reserved = 0;
+                    }
                 }
-            }
-            if(structure.structureType == STRUCTURE_CONTAINER) {
-                if(structure.memory.reserved == undefined) {
-                    structure.memory.reserved = 0;
+                if(structure.structureType == STRUCTURE_CONTAINER) {
+                    if(structure.memory.reserved == undefined) {
+                        structure.memory.reserved = 0;
+                    }
+                    Memory.containers.push(structure.id);
                 }
-                Memory.containers.push(structure.id);
             }
         }
     }
