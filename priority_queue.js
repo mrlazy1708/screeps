e306f39671bfbed28b150f8fdca8403595d9a720
Yiheng Du <15700066418@163.com>
2020-06-06 20:17:04 +0800

    fix priority queue bug

--- priority_queue.js
+++ priority_queue.js
@@ -1,19 +1,28 @@
+function cmp(i,j){
+    if(i.time==j.time)return i.pri<j.pri;
+    return i.time<j.time;
+}
+
+function min(i,j){
+    return cmp(i,j)?i:j;
+}
+
 const pq = {
     top: function(heap){
         return heap[1];
     },
     insert: function(heap, value){
         heap.push(value);
-        for(let i=heap.length-1;i>1&&heap.cmp(i,i>>1);i>>=1)[heap[i],heap[i>>1]]=[heap[i>>1],heap[i]];
+        for(let i=heap.length-1;i>1&&cmp(heap[i],heap[i>>1]);i>>=1)[heap[i],heap[i>>1]]=[heap[i>>1],heap[i]];
     },
     remove: function(heap){
         let i;
         if(heap.length>2)heap[1]=heap.pop();
         else heap.pop();
-        for(i=1;i<<1<heap.length-1&&!heap.cmp(i,heap.min(i<<1,i<<1|1));)
-            if(heap.cmp(i<<1,i<<1|1))[heap[i],heap[i<<1]]=[heap[i<<1],heap[i]],i=i<<1;
+        for(i=1;i<<1<heap.length-1&&!cmp(heap[i],min(heap[i<<1],heap[i<<1|1]));)
+            if(cmp(heap[i<<1],heap[i<<1|1]))[heap[i],heap[i<<1]]=[heap[i<<1],heap[i]],i=i<<1;
             else [heap[i],heap[i<<1|1]]=[heap[i<<1|1],heap[i]],i=i<<1|1;
-        if(i<<1==heap.length-1&&!heap.cmp(i,i<<1))[heap[i],heap[i<<1]]=[heap[i<<1],heap[i]];
+        if(i<<1==heap.length-1&&!cmp(heap[i],heap[i<<1]))[heap[i],heap[i<<1]]=[heap[i<<1],heap[i]];
     }
 };
 
