0401a0df5f5d62b0358b9d7727dc6000b6622dd8
Yiheng Du <15700066418@163.com>
2020-06-06 15:47:47 +0800

    fix bug

--- structure.container.js
+++ structure.container.js
@@ -1,5 +1,8 @@
 const structureContainer = {
     run: function(container) {
+        if(container.memory.reserved == undefined) {
+            container.memory.reserved = 0;
+        }
         let sum = container.memory.reserved - container.store[RESOURCE_ENERGY];
         if(sum < -100) {
             global.sources.insert({time: 1, pri: sum, hostID: container.id});
--- structure.tower.js
+++ structure.tower.js
@@ -1,6 +1,8 @@
 const structureTower = {
     run: function(tower) {
-        
+        if(tower.memory.reserved == undefined) {
+            tower.memory.reserved = 0;
+        }
         if(tower.memory.state == 'fill') {
             if(tower.store.getFreeCapacity(RESOURCE_ENERGY) == 0) {
                 tower.memory.state = 'full';
