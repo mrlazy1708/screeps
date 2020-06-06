ec618901641ddedd2f2917726e53c822a45f9e0e
Yiheng Du <15700066418@163.com>
2020-06-06 16:42:30 +0800

    fix bug

--- task.structure.js
+++ task.structure.js
@@ -4,8 +4,9 @@ const structureTower = require('structure.tower');
 const taskStructure = {
     run: function() {
         let structures = _.groupBy(Game.structures, (structure)=>{return structure.structureType;});
-        for(let id in structures[STRUCTURE_CONTAINER]) {
-            structureContainer.run(Game.structures[id]);
+        for(let id in Memory.containers) {
+        	let container = Game.getObjectById(Memory.containers);
+            structureContainer.run(container);
         }
         for(let id in structures[STRUCTURE_TOWER]) {
             structureTower.run(Game.structures[id]);
