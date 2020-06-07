5913376f40b2bf7eb473205cd4eb74c28a5310d7
Yiheng Du <15700066418@163.com>
2020-06-07 16:51:03 +0800

    fix room designer

--- task.design.js
+++ task.design.js
@@ -15,6 +15,7 @@ const taskDesign = {
                 for(let i=0;i<4;i++){
                     let n={x:c.x+dx[i],y:c.y+dy[i],s:c.s+1};
                     if(terrain.get(n.x,n.y)!=TERRAIN_MASK_WALL&&maze[n.x][n.y]==undefined){
+                        if(tot[n.x][n.y]==undefined)tot[n.x][n.y]=0;
                         tot[n.x][n.y]+=(maze[n.x][n.y]=n.s);
                         q.push(n);
                     }
@@ -22,7 +23,7 @@ const taskDesign = {
             }
         }
         if(room.controller!=undefined)
-            bfs(room.comtroller.pos);
+            bfs(room.controller.pos);
         let sources=room.find(FIND_SOURCES);
         for(let id in sources)
             bfs(sources[id].pos);
@@ -31,7 +32,7 @@ const taskDesign = {
             for(let y=0;y<50;y++)
                 if(tot[x][y]<centp.s)
                     centp={x:x,y:y,s:tot[x][y]};
-        console.log(centp);
+        console.log(JSON.stringify(centp));
     }
 };
 
