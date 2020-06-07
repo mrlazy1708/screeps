32604d2c4fd7a2c314ad0910eb8a582abfc55a2b
Yiheng Du <15700066418@163.com>
2020-06-07 16:44:48 +0800

    add room designer

--- task.design.js
+++ task.design.js
@@ -1,28 +1,37 @@
 const taskDesign = {
     run: function(room) {
-        let centp, pos = [room.controller.pos], sources = room.find(FIND_SOURCES), minerals = room.find(FIND_MINERALS);
-        for(let id in sources) pos.push(sources[id].pos);
-        for(let id in minerals) pos.push(minerals[id].pos);
-        for(let i=1; i<=100; i++) {
-            let cx = 0, cy = 0;
-            for(let id in pos) {
-                cx += pos[id].x;
-                cy += pos[id].y;
-            }
-            cx = Math.round(cx / pos.length);
-            cy = Math.round(cy / pos.length);
-            centp = new RoomPosition(cx, cy, room.name);;
-            for(let id in pos) {
-                let next = pos[id].findPathTo(centp)[0];
-                pos[id] = new RoomPosition(next.x, next.y, room.name);
+        let tot=[],dx=[1,-1,0,0],dy=[0,0,1,-1],terrain=room.getTerrain();
+        for(let x=0;x<50;x++){
+            tot[x]=[];
+            for(let y=0;y<50;y++)
+                tot[x][y]=0;
+        }
+        function bfs(pos){
+            let maze=[];
+            for(let x=0;x<50;x++)
+                maze[x]=[];
+            for(let q=[{x:pos.x,y:pos.y,s:0}];q.length;){
+                let c=q.shift();
+                for(let i=0;i<4;i++){
+                    let n={x:c.x+dx[i],y:c.y+dy[i],s:c.s+1};
+                    if(terrain.get(n.x,n.y)!=TERRAIN_MASK_WALL&&maze[n.x][n.y]==undefined){
+                        tot[n.x][n.y]+=(maze[n.x][n.y]=n.s);
+                        q.push(n);
+                    }
+                }
             }
         }
-        for(let id in sources) {
-            for(let pos = sources[id].pos; pos != centp; ) {
-                let next = pos.findPathTo(centp)[0];
-                room.createConstructionSite(pos = new RoomPosition(next.x, next.y, room.name), STRUCTURE_ROAD);
-            }
-        }
+        if(room.controller!=undefined)
+            bfs(room.comtroller.pos);
+        let sources=room.find(FIND_SOURCES);
+        for(let id in sources)
+            bfs(sources[id].pos);
+        let centp={s:1e9+7};
+        for(let x=0;x<50;x++)
+            for(let y=0;y<50;y++)
+                if(tot[x][y]<centp.s)
+                    centp={x:x,y:y,s:tot[x][y]};
+        console.log(centp);
     }
 };
 
