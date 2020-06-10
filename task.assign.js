d3e4d1424c2c1a9b1e1d11a539f9cf1b56c1a117
Yiheng Du <mrlazy1708@gmail.com>
2020-06-10 10:38:38 +0800

    fix stupid bug

--- task.assign.js
+++ task.assign.js
@@ -6,7 +6,7 @@ function assignTrans(tasks, creeps, val) {
     for(let task; (task = tasks.Top()) != undefined && task.time <= Game.time; ) {
         let host = Game.getObjectById(task.hostID);
         if(host != undefined){ 
-            let index = host.pos.Find(creep);
+            let index = host.pos.Find(creeps);
             if(index != null) {
                 let creep = Game.getObjectById(creeps[index]);
                 mDelete(creeps, index);
