98cdeb7668e929ac2d012d5cfc8b17e1aece0682
Yiheng Du <mrlazy1708@gmail.com>
2020-06-10 19:07:46 +0800

    fix spawn bug

--- task.spawn.js
+++ task.spawn.js
@@ -3,12 +3,14 @@ const roleSpawn = require('role.spawn');
 const taskSpawn = {
     run: function() {
         for(let task; (task = Memory.task.spawn.Top()) != undefined && task.time <= Game.time; ) {
-            let index = task.pos.Find(spawns);
+            let index = task.pos.Find(Memory.idleSpawn);
             if(index != null) {
-                let spawn = spawns[index];
-                nDelete(spawns, index);
-                Memory.task.spawn.Pop();
-                spawn.memory.task = task.disc;
+                let spawn = Game.getObjectById(Memory.idleSpawn[index]);
+                Memory.idleSpawn.Delete(index);
+                if(spawn) {
+                    Memory.task.spawn.Pop();
+                    spawn.memory.task = task.disc;
+                }
             }
             else break;
         }
@@ -17,6 +19,7 @@ const taskSpawn = {
             let spawn = Game.spawns[name];
             if(!spawn.spawning && roleSpawn.run(spawn) == OK) {
                 spawn.memory.task = null;
+                Memory.idleSpawn.Push(spawn.id);
             }
         }
 	}
